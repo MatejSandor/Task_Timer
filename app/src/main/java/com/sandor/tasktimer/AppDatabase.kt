a67a76c68bf3d993823f50fec7fadd3eaa968acb
Matej Šándor <matejko.sandor@gmail.com>
2020-05-14 11:27:03 +0200

    create SQL for Tasks table

--- app/src/main/java/com/sandor/tasktimer/AppDatabase.kt
+++ app/src/main/java/com/sandor/tasktimer/AppDatabase.kt
@@ -3,6 +3,7 @@ package com.sandor.tasktimer
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 private const val TAG = "AppDatabase"
 
@@ -12,11 +13,22 @@ private const val DATABASE_VERSION = 1
 internal class AppDatabase constructor(context: Context) : SQLiteOpenHelper
     (context, DATABASE_NAME, null, DATABASE_VERSION) {
 
-    override fun onCreate(db: SQLiteDatabase?) {
+    override fun onCreate(db: SQLiteDatabase) {
         // CREATE TABLE Tasks (_id INTEGER PRIMARY KEY NOT NULL, Name TEXT NOT NULL, Description TEXT, SortOrder INTEGER);
+        Log.d(TAG,"onCreate: starts")
+        val sSql = """CREATE TABLE ${TasksContract.TABLE_NAME}(
+            ${TasksContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
+            ${TasksContract.Columns.TASK_NAME} TEXT NOT NULL,
+            ${TasksContract.Columns.TASK_DESCRIPTION} TEXT,
+            ${TasksContract.Columns.TASK_SORT_ORDER} INTEGER
+            ); """.replaceIndent(" ")
+
+        Log.d(TAG,"onCreate: table created $sSql")
+        db.execSQL(sSql)
+
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
 //        TODO()
     }
 
