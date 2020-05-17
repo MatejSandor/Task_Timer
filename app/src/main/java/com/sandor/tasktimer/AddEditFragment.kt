9b574aea0ab39744adece70becf0835f588cca19
Matej Šándor <matejko.sandor@gmail.com>
2020-05-17 10:56:06 +0200

    changed generated code in AddEditFragment

--- app/src/main/java/com/sandor/tasktimer/AddEditFragment.kt
+++ app/src/main/java/com/sandor/tasktimer/AddEditFragment.kt
@@ -1,6 +1,8 @@
 package com.sandor.tasktimer
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_TASK = "task"
+private const val TAG = "AddEditFragment"
 
 /**
  * A simple [Fragment] subclass.
@@ -16,18 +19,41 @@ private const val ARG_TASK = "task"
  */
 class AddEditFragment : Fragment() {
     private var task: Task? = null
+    private var listener: OnSaveClicked? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
-        task = arguments?.getParcelable<Task>(ARG_TASK)
+        task = arguments?.getParcelable(ARG_TASK)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(TAG, "onCreateView: starts")
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit2, container, false)
+        return inflater.inflate(R.layout.fragment_add_edit, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG, "onAttach: starts")
+        super.onAttach(context)
+        if (context is OnSaveClicked) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnSaveClicked")
+        }
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach: starts")
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnSaveClicked {
+        fun onSaveClicked()
     }
 
     companion object {
@@ -35,16 +61,15 @@ class AddEditFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param task
          * @return A new instance of fragment AddEditFragment.
          */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(task: Task?) =
             AddEditFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_TASK, param1)
+                    putParcelable(ARG_TASK, task)
                 }
             }
     }
--- app/src/main/java/com/sandor/tasktimer/MainActivityFragment.kt
+++ app/src/main/java/com/sandor/tasktimer/MainActivityFragment.kt
@@ -1,22 +1,18 @@
 package com.sandor.tasktimer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 /**
- * A simple [Fragment] subclass.
+ * A placeholder fragment containing a simple view.
  */
 class MainActivityFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
-
 }
