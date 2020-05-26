2c13b3424dff2a791c19bd68117a99ca5d0fc964
Matej Šándor <matejko.sandor@gmail.com>
2020-05-26 18:27:26 +0200

    add interface for dialog events

--- app/src/main/java/com/sandor/tasktimer/AppDialog.kt
+++ app/src/main/java/com/sandor/tasktimer/AppDialog.kt
@@ -1,9 +1,16 @@
 package com.sandor.tasktimer
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatDialogFragment
 
 private const val TAG = "AppDialog"
 
 class AppDialog : AppCompatDialogFragment() {
+
+    internal interface DialogEvents {
+        fun onPositiveDialogResult(dialogId: Int, args: Bundle)
+        fun onNegativeDialogResult(dialogId: Int, args: Bundle)
+        fun onDialogCancelled(dialogId: Int)
+    }
     
 }
