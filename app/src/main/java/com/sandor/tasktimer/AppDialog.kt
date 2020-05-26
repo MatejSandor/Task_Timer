8a285ba753cab8ac2cb987c687cdca7efe386e8d
Matej Šándor <matejko.sandor@gmail.com>
2020-05-26 18:24:13 +0200

    add class for dialogs

---
+++ app/src/main/java/com/sandor/tasktimer/AppDialog.kt
@@ -0,0 +1,9 @@
+package com.sandor.tasktimer
+
+import androidx.appcompat.app.AppCompatDialogFragment
+
+private const val TAG = "AppDialog"
+
+class AppDialog : AppCompatDialogFragment() {
+    
+}
